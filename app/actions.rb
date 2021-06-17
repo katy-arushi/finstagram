45e07a08369e2f486f340acf11b88ebb6a02a487
Arushi Katyal <katy.arushi@gmail.com>
2021-06-17 00:51:18 +0000

    W5D2

--- app/actions.rb
+++ app/actions.rb
@@ -37,10 +37,10 @@ post '/login' do
     username = params[:username]
     password = params[:password]
 
-    @user = User.find_by(username: username)
+    user = User.find_by(username: username)
 
     if user && user.password == password
-        session[:user_id] = @user.id
+        session[:user_id] = user.id
         redirect to('/')
     else
         @error_message = "Login failed."
@@ -51,4 +51,26 @@ end
 get '/logout' do
     session[:user_id] = nil
     redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+    if @finstagram_post.save
+        redirect(to('/'))
+    else
+        erb(:"finstagram_posts/new")
+    end
+end
+
+get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id])
+    erb(:"finstagram_posts/show")
 end
